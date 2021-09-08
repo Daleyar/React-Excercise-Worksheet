f5a2d011506ef6e0bc47f53d606158aed32ef48c
Ali Daley <ali.daley72@gmail.com>
2021-09-08 17:21:03 -0400

    Completed Component Excercise 1 by Creating Display Name Component and Rendering in the Page.

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import DisplayName from './DisplayName/DisplayName';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: 'Reggie',
+            lastName: 'White'
+        }
+    }
+
+    render() { 
+        return ( 
+            <div className='container-fluid'>
+                <h1>
+                <DisplayName firstName={this.state.firstName} />
+                <DisplayName lastName={this.state.lastName} />
+                </h1>
+            </div>
+        );
+    }
+}
+ 
+export default App;
